0ee05e4df938f5911eda3251c9cec546b652bbe6
久保田悦也 <instrctr.etsuya@gmail.com>
2022-01-21 20:31:51 +0900

    add: ルーティングの設定

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,15 +1,20 @@
 import HelloReact from "./HelloReact";
 import CountUpDown from "./CountUpDown";
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 
 const Navigation = () => {
   return (
     <>
+    <BrowserRouter>
       <div>
-        <HelloReact />
+        <Link to={'/'}>HelloReact</Link>
+        <Link to={'/countUpDown'}>CountUpDown</Link>
       </div>
-      <div>
-        <CountUpDown />
-      </div>
+      <Routes>
+        <Route path={'/'} element={<HelloReact />} />
+        <Route path={'/countUpDown'} element={<CountUpDown />} />
+      </Routes>
+    </BrowserRouter>
     </>
   )
 };
