512ad46571abd246fcd3467d13e9880bab544fac
久保田悦也 <instrctr.etsuya@gmail.com>
2022-01-25 11:41:03 +0900

    add: バリデーションの追加

--- client/src/components/account/SignUp.jsx
+++ client/src/components/account/SignUp.jsx
@@ -1,18 +1,9 @@
 import SignUpConfirmation from "./SignUpConfirmation";
 import { useState } from "react";
 import axios from "axios";
+import { useForm } from "react-hook-form";
 
 const SignUp = () => {
-  //formDataの状態管理
-  const [formData, setFormData] = useState({
-    first_name:            "",
-    last_name:             "",
-    email:                 "",
-    phone:                 "",
-    password:              "",
-    password_confirmation: "",
-  }
-);
   // 確認モーダルの状態管理
   const [flgConfirmation, setFlgConfirmation] = useState(false);
 
@@ -25,30 +16,29 @@ const SignUp = () => {
   // ApiEndPoint アカウント登録
   const url = "http://localhost:3001/api/v1/auth";
 
-  // アカウント登録用データ
-  const postData = {
-    first_name:            formData.first_name,
-    last_name:             formData.last_name,
-    email:                 formData.email,
-    phone:                 formData.phone,
-    password:              formData.password,
-    password_confirmation: formData.password_confirmation,
-  }
+  // React Hook Form オブジェクト
+  const { register, handleSubmit, watch, formState: { errors }, getValues } = useForm();
 
-  // formData Changeイベント
-  const changeFirstName            = e => setFormData({...formData, first_name: e.target.value});
-  const changeLastName             = e => setFormData({...formData, last_name: e.target.value});
-  const changeEmail                = e => setFormData({...formData, email: e.target.value});
-  const changePhone                = e => setFormData({...formData, phone: e.target.value});
-  const changePassword             = e => setFormData({...formData, password: e.target.value});
-  const changePasswordConfirmation = e => setFormData({...formData, password_confirmation: e.target.value});
+  // 各インプットの状態管理
+  const watchData = watch({
+    first_name:            "first_name",
+    last_name:             "last_name",
+    email:                 "email",
+    phone:                 "phone",
+    password:              "password",
+    password_confirmation: "password_confirmation",
+  });
+
+  // 正規表現
+  const checkEmail = /^[A-Za-z0-9]{1}[A-Za-z0-9_.-]*@{1}[A-Za-z0-9_.-]{1,}.[A-Za-z0-9]{1,}$/;
+  const checkPassword = /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]{5,100}$/;
 
   // 確認モーダルのトグルイベント
   const toggleFlgConfirmation = e => setFlgConfirmation(!flgConfirmation);
 
   // アカウント登録イベント
-  const accuntRegistration = () => {
-    axios.post(url, postData, headers)
+  const accountRegistration = () => {
+    axios.post(url, watchData, headers)
     .then(res => {
       alert("登録が成功しました")
       window.location.href("http://ocalhost:3001/sign-up");
@@ -59,33 +49,77 @@ const SignUp = () => {
     })
   }
 
-  const register = () => {
-    accuntRegistration();
+  const accountRegister = () => {
+    accountRegistration();
   }
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(toggleFlgConfirmation)}>
       <label htmlFor="first_name">姓 :</label>
-      <input type="text" name="first_name" value={formData.first_name} onChange={changeFirstName} /><br/>
+      <input
+        type="text"
+        name="first_name"
+        {...register("first_name", {required: "姓は必須です。"})}
+      />
+      {errors.first_name && <span>{errors.first_name.message}</span>}<br/>
 
       <label htmlFor="last_name">名 :</label>
-      <input type="text" name="last_name" value={formData.last_name} onChange={changeLastName} /><br/>
+      <input
+        type="text"
+        name="last_name"
+        {...register("last_name", {required: "名は必須です。"})}
+      />
+        {errors.last_name && <span>{errors.last_name.message}</span>}<br/>
 
       <label htmlFor="email">メールアドレス :</label>
-      <input type="text" name="email" value={formData.email} onChange={changeEmail} /><br/>
+      <input
+        type="text"
+        name="email"
+        {...register("email", {
+            required: "メールアドレスは必須です。正しく入力してください。",
+            pattern: {
+              value: checkEmail,
+              message: "メールアドレスには@が必須です。",
+            }
+          }
+        )}
+      />
+      {errors.email && <span>{errors.email.message}</span>}<br/>
 
       <label htmlFor="phone">電話番号 :</label>
-      <input type="text" name="phone" value={formData.phone} onChange={changePhone} /><br/>
+      <input
+        type="text"
+        name="phone"
+        {...register("phone", {required: "電話番号は必須です。"})}
+      />
+      {errors.phone && <span>{errors.email.message}</span>}<br/>
 
       <label htmlFor="password">パスワード :</label>
-      <input type="text" name="password" value={formData.password} onChange={changePassword} /><br/>
+      <input
+        type="text"
+        name="password"
+        {...register("password", {
+            required: "パスワードは必須です",
+            pattern: {
+              value: checkPassword,
+              message: "半角英数字&大文字と小文字を含めてください。"
+            }
+          }
+        )}
+      />
+      {errors.password && <span>{errors.password.message}</span>}<br/>
 
       <label htmlFor="password_confirmation">確認用パスワード :</label>
-      <input type="text" name="password_confirmation" value={formData.password_confirmation} onChange={changePasswordConfirmation} /><br/>
+      <input
+      type="text"
+      name="password_confirmation"
+      {...register("password_confirmation", {required: "確認用パスワードは必須です。", validate: value => value === getValues('password') || <span>パスワードが一致しません。</span>})}
+      />
+      {errors.password_confirmation && <span>{errors.password_confirmation.message}</span>}<br/>
 
-      {!flgConfirmation && <button type="button" onClick={toggleFlgConfirmation}>確認</button>}
+      {!flgConfirmation && <button type="submit">確認</button>}
       {flgConfirmation  && <button type="button" onClick={toggleFlgConfirmation}>修正</button>}
-      {flgConfirmation  && <button type="button" onClick={register}>登録</button>}
+      {flgConfirmation  && <button type="button" onClick={accountRegister}>登録</button>}
     </form>
   )
 };
